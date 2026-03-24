67112d882be6c3c767199bd3920ec931130c0930
Matthieu Hardouin <matthieu.hardouin.pro@gmail.com>
2026-03-24 15:20:34 +0100

    feat: Tailwind CSS installé en local sans CDN + watch mode

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use App\Repository\ContactRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -25,6 +26,19 @@ class ContactController extends AbstractController
             $em->persist($contact);
             $em->flush();
 
+            $email = (new Email())
+                ->from('portfolio@localhost.com')
+                ->to('matthieu@localhost.com')
+                ->subject('Nouveau message de ' . $contact->getName())
+                ->html('
+                    <h2>Nouveau message reçu</h2>
+                    <p><strong>Nom :</strong> ' . $contact->getName() . '</p>
+                    <p><strong>Email :</strong> ' . $contact->getEmail() . '</p>
+                    <p><strong>Message :</strong><br>' . nl2br($contact->getMessage()) . '</p>
+                ');
+
+            $mailer->send($email);
+            
             $this->addFlash('success', 'Votre message a bien été envoyé !');
 
             return $this->redirectToRoute('app_contact');
--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 cd C:\laragon\www\portfolio 
+start cmd /k "npx tailwindcss -i ./assets/styles/app.css -o ./public/styles/app.css --watch"
 symfony server:start --daemon 
 start http://127.0.0.1:8000 
